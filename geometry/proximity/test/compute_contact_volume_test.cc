3c6141d42b8d4b1b15af9528e6c63a0f257e43da
DamrongGuoy <Damrong.Guoy@tri.global>
2024-12-10 11:05:31 -0800

    Delete the old version of ComputeContactVolume()

--- geometry/proximity/test/compute_contact_volume_test.cc
+++ geometry/proximity/test/compute_contact_volume_test.cc
@@ -47,14 +47,28 @@ using std::vector;
 class ComputeContactVolumeTest : public ::testing::Test {
  public:
   ComputeContactVolumeTest()
-      : box_mesh0_M_(MakeBoxVolumeMeshWithMa<double>(box_)),
+      :  // Data for the box.
+        box_mesh0_M_(MakeBoxVolumeMeshWithMa<double>(box_)),
         box_bvh0_M_(box_mesh0_M_),
+        box_boundary_M_(box_mesh0_M_),
+        box_hydro_compliant_geometry_(hydroelastic::SoftMesh(
+            std::make_unique<VolumeMesh<double>>(box_mesh0_M_),
+            std::make_unique<VolumeMeshFieldLinear<double, double>>(
+                MakeBoxPressureField(box_, &box_mesh0_M_,
+                                     box_hydro_modulus_)))),
+        // Data for the octahedron.
         // Get a mesh of an octahedron from a sphere specification by
         // specifying very coarse resolution hint.
         octahedron_mesh1_N_(MakeSphereVolumeMesh<double>(
             sphere_, 10 * sphere_.radius(),
             TessellationStrategy::kSingleInteriorVertex)),
-        octahedron_bvh1_N_(octahedron_mesh1_N_) {}
+        octahedron_bvh1_N_(octahedron_mesh1_N_),
+        octahedron_boundary_N_(octahedron_mesh1_N_),
+        octahedron_hydro_compliant_geometry_(hydroelastic::SoftMesh(
+            std::make_unique<VolumeMesh<double>>(octahedron_mesh1_N_),
+            std::make_unique<VolumeMeshFieldLinear<double, double>>(
+                MakeSpherePressureField(sphere_, &octahedron_mesh1_N_,
+                                        octahedron_hydro_modulus_)))) {}
 
  protected:
   void SetUp() override {
@@ -63,13 +77,19 @@ class ComputeContactVolumeTest : public ::testing::Test {
 
   // Geometry 0 and its field.
   const Box box_{0.06, 0.10, 0.14};  // 6cm-thick compliant pad.
+  const double box_hydro_modulus_{1e7};
   const VolumeMesh<double> box_mesh0_M_;
   const Bvh<Obb, VolumeMesh<double>> box_bvh0_M_;
+  const MeshDistanceBoundary box_boundary_M_;
+  const hydroelastic::SoftGeometry box_hydro_compliant_geometry_;
 
   // Geometry 1 and its field.
   const Sphere sphere_{0.03};  // 3cm-radius (6cm-diameter) finger tip.
+  const double octahedron_hydro_modulus_{1e7};
   const VolumeMesh<double> octahedron_mesh1_N_;
   const Bvh<Obb, VolumeMesh<double>> octahedron_bvh1_N_;
+  const MeshDistanceBoundary octahedron_boundary_N_;
+  const hydroelastic::SoftGeometry octahedron_hydro_compliant_geometry_;
 };
 
 TEST_F(ComputeContactVolumeTest, ComputeContactVolume) {
@@ -86,52 +106,48 @@ TEST_F(ComputeContactVolumeTest, ComputeContactVolume) {
 
     std::pair<std::unique_ptr<ContactSurface<double>>,
               std::unique_ptr<ContactSurface<double>>>
-        pair = ComputeContactVolume(
-            first_id, box_mesh0_M_, box_bvh0_M_, X_WM, second_id,
-            octahedron_mesh1_N_, octahedron_bvh1_N_, X_WN,
-            HydroelasticContactRepresentation::kTriangle);
+        pair = ComputeContactVolume(first_id, box_boundary_M_, X_WM, second_id,
+                                    octahedron_boundary_N_, X_WN,
+                                    // Extra parameters because we do not have
+                                    // triangle-triangle mesh clipping yet.
+                                    box_hydro_compliant_geometry_,
+                                    octahedron_hydro_compliant_geometry_);
 
     {
       SCOPED_TRACE("First contact boundary.");
       ASSERT_NE(pair.first.get(), nullptr);
       const ContactSurface<double>& s1 = *pair.first.get();
       ASSERT_GT(s1.num_vertices(), 0);
-      ASSERT_TRUE(s1.is_triangle());
-      const std::vector<double>& distances = s1.tri_e_MN().values();
-      EXPECT_NEAR(*std::min_element(distances.begin(), distances.end()), 0,
+      ASSERT_FALSE(s1.is_triangle());
+      const std::vector<double>& distances = s1.poly_e_MN().values();
+      EXPECT_NEAR(*std::min_element(distances.begin(), distances.end()),
+                  -0.01 * std::sqrt(3.0), 1e-14);
+      EXPECT_NEAR(*std::max_element(distances.begin(), distances.end()), 0,
                   1e-14);
-      EXPECT_NEAR(*std::max_element(distances.begin(), distances.end()), 0.03,
-                  1e-14);
-      WriteTriangleSurfaceMeshFieldLinearToVtk("first_boundary.vtk", "distance",
-                                               s1.tri_e_MN(),
-                                               "compute_contact_volume");
+      // We don't have writing polygonal data to VTK yet. We only have it for
+      // triangles.
+      // WriteTriangleSurfaceMeshFieldLinearToVtk("first_boundary.vtk",
+      //                                          "distance",
+      //                                          s1.tri_e_MN(),
+      //                                          "compute_contact_volume");
     }
     {
       SCOPED_TRACE("Second contact boundary.");
       ASSERT_NE(pair.second.get(), nullptr);
       const ContactSurface<double>& s2 = *pair.second.get();
       ASSERT_GT(s2.num_vertices(), 0);
-      ASSERT_TRUE(s2.is_triangle());
-      const std::vector<double>& distances = s2.tri_e_MN().values();
-      EXPECT_NEAR(*std::min_element(distances.begin(), distances.end()), 0,
+      ASSERT_FALSE(s2.is_triangle());
+      const std::vector<double>& distances = s2.poly_e_MN().values();
+      EXPECT_NEAR(*std::min_element(distances.begin(), distances.end()), -0.03,
                   1e-14);
-      EXPECT_NEAR(*std::max_element(distances.begin(), distances.end()),
-                  0.01 * std::sqrt(3.0), 1e-14);
-      WriteTriangleSurfaceMeshFieldLinearToVtk("second_boundary.vtk",
-                                               "distance", s2.tri_e_MN(),
-                                               "compute_contact_volume");
+      EXPECT_NEAR(*std::max_element(distances.begin(), distances.end()), 0,
+                  1e-14);
+      // We don't have writing polygonal data to VTK yet. We only have it for
+      // triangles.
+      // WriteTriangleSurfaceMeshFieldLinearToVtk("second_boundary.vtk",
+      //                                          "distance", s2.tri_e_MN(),
+      //                                          "compute_contact_volume");
     }
-  }
-  {
-    SCOPED_TRACE("Request polygons.");
-    std::pair<std::unique_ptr<ContactSurface<double>>,
-              std::unique_ptr<ContactSurface<double>>>
-        pair = ComputeContactVolume(
-            first_id, box_mesh0_M_, box_bvh0_M_, X_WM, second_id,
-            octahedron_mesh1_N_, octahedron_bvh1_N_, X_WN,
-            HydroelasticContactRepresentation::kPolygon);
-    ASSERT_NE(pair.first.get(), nullptr);
-    ASSERT_NE(pair.second.get(), nullptr);
   }
 }
 
