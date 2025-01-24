7a6f1c9f29c4018661d933e6543f2d03cc018a5a
DamrongGuoy <Damrong.Guoy@tri.global>
2025-01-23 17:18:11 -0800

    Add VegaCdt(): drake's TriangleSurfaceMesh -> drake's VolumeMesh

--- tmp/vega_mesh_to_drake_mesh.h
+++ tmp/vega_mesh_to_drake_mesh.h
@@ -1,14 +1,21 @@
 #pragma once
 
 #include "drake/geometry/proximity/volume_mesh.h"
-#include "tetMesh.h"
+#include "drake/geometry/proximity/triangle_surface_mesh.h"
 
+#include "objMesh.h"
+#include "tetMesh.h"
 
 namespace drake {
 namespace geometry {
 
 VolumeMesh<double> VegaFemTetMeshToDrakeVolumeMesh(
     const vegafem::TetMesh& vega_mesh);
+
+vegafem::ObjMesh DrakeTriangleSurfaceMeshToVegaObjMesh(
+    const TriangleSurfaceMesh<double>& drake_mesh);
+
+VolumeMesh<double> VegaCdt(const TriangleSurfaceMesh<double>& surface_mesh);
 
 }  // namespace geometry
 }  // namespace drake
