ee42ea1b256d150107c673874dd58edbda9e174b
DamrongGuoy <Damrong.Guoy@tri.global>
2025-01-27 08:03:52 -0800

    Factor out vega_cdt as its own module to hide VegaFEM dependency

--- tmp/test/vega_mesh_to_drake_mesh_test.cc
+++ tmp/test/vega_mesh_to_drake_mesh_test.cc
@@ -60,22 +60,6 @@ GTEST_TEST(DrakeTriangleSurfaceMeshToVegaObjMeshTest, Tetrahedron) {
   EXPECT_EQ(obj_mesh.getNumFaces(), 4);
 }
 
-GTEST_TEST(VegaCdtTest, Tetrahedron) {
-  // A four-triangle mesh of a standard tetrahedron.
-  const TriangleSurfaceMesh<double> drake_surface_mesh{
-      {// The triangle windings give outward normals.
-       SurfaceTriangle{0, 2, 1}, SurfaceTriangle{0, 1, 3},
-       SurfaceTriangle{0, 3, 2}, SurfaceTriangle{1, 2, 3}},
-      {Vector3d::Zero(), Vector3d::UnitX(), Vector3d::UnitY(),
-       Vector3d::UnitZ()}};
-
-  VolumeMesh<double> drake_tetrahedral_mesh = VegaCdt(drake_surface_mesh);
-
-  // Expect a one-tetrahedron mesh with four vertices.
-  EXPECT_EQ(drake_tetrahedral_mesh.num_vertices(), 4);
-  EXPECT_EQ(drake_tetrahedral_mesh.num_elements(), 1);
-}
-
 }  // namespace
 }  // namespace geometry
 }  // namespace drake
